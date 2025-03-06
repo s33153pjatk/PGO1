e946fa544bf294fd0c387e3d80a9f133ec20beb0
Bartosz Aftyka <s33153@pjwstk.edu.pl>
2025-03-06 11:04:02 +0100

    Zmiana 1

--- PGO1/src/Main.java
+++ PGO1/src/Main.java
@@ -1,5 +1,10 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        int a = 1;
+        int b = 2;
+        int c = a + b;
+        System.out.println(c);
     }
+
 }
